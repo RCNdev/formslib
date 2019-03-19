0045acd2c591f371a4199b746c04d666cd9fe201
rcnsheppardp <peter.sheppard@rcn.org.uk>
2019-03-19 16:00:40 +0000

    Add GUID field type

---
+++ src/Field/Guid.php
@@ -0,0 +1,12 @@
+<?php
+namespace formslib\Field;
+
+class Guid extends Field
+{
+	public function __construct($name)
+	{
+		parent::__construct($name);
+		$this->addRule('maxlength', 38, 'GUIDs cannot be this long');
+		$this->addRule('regex', '/^\{?[A-Z0-9]{8}-[A-Z0-9]{4}-[A-Z0-9]{4}-[A-Z0-9]{4}-[A-Z0-9]{12}\}?$/i', 'Please make sure you enter a valid GUID with correctly-placed hyphens');
+	}
+}
