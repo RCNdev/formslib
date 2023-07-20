2011312ffb4ff18e9eaf157d2eaab2973c05bde6
Peter Sheppard <Peter.Sheppard@rcn.org.uk>
2023-07-20 17:33:35 +0100

    Implement IN display condition

--- src/Form.php
+++ src/Form.php
@@ -1284,8 +1284,20 @@ JS;
 				break;
 
 			case \formslib\Operator::IN:
+
+				$values = json_encode($value);
+
 				$jq .= <<<JS
-// TODO: Conditional display on IN
+	var values = $values;
+
+	if (vals.contains(fld.val()))
+	{
+		$('[data-formslib-owner="{$type}_$id"]').show();
+	}
+	else
+	{
+		$('[data-formslib-owner="{$type}_$id"]').hide();
+	}
 JS;
 				break;
 
