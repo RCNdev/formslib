4dac0b13d12b850304d9b57bba096a0f0a4c0d5e
Peter Sheppard <Peter.Sheppard@rcn.org.uk>
2024-03-15 15:31:57 +0000

    Add custom format option to DateSelect

--- src/Field/DateSelect.php
+++ src/Field/DateSelect.php
@@ -3,19 +3,29 @@ namespace formslib\Field;
 
 class DateSelect extends \formslib_select
 {
-	protected $dateformat;
+	protected $dateformat = '';
 
-	public function &setDateRange($startDate, $endDate)
+	/**
+	 * Set the date range for the dropdown
+	 *
+	 * @param string $startDate Start date as a string that can be passed to \DateTime
+	 * @param string $endDate End date as a string that can be passed to \DateTime
+	 * @param string $customFormat
+	 *
+	 * @return $this
+	 */
+	public function &setDateRange($startDate, $endDate, $customFormat = null)
 	{
 		$patternDate = '/[0-9]{4}-[0-9]{2}-[0-9]{2}/';
+
 		if (!preg_match($patternDate, $startDate) || !preg_match($patternDate, $endDate))
 		{
-			throw new \Exception('Invalid date format');
+			throw new \UnexpectedValueException('Invalid date format');
 		}
 
 		if ($startDate > $endDate)
 		{
-			throw new \Exception('End date constraint before start date');
+			throw new \UnexpectedValueException('End date constraint before start date');
 		}
 
 		$start = new \DateTime($startDate);
@@ -24,9 +34,22 @@ class DateSelect extends \formslib_select
 		$interval = $start->diff($end);
 		$daycount = $interval->days;
 
-		if ($daycount < 5) $this->dateformat = 'l';
-		elseif ($daycount < 21) $this->dateformat = 'l jS';
-		elseif ($daycount >= 21) $this->dateformat = 'l j M';
+		if (!is_null($customFormat))
+		{
+			$this->dateformat = $customFormat;
+		}
+		elseif ($daycount < 5)
+		{
+			$this->dateformat = 'l';
+		}
+		elseif ($daycount < 21)
+		{
+			$this->dateformat = 'l jS';
+		}
+		elseif ($daycount >= 21)
+		{
+			$this->dateformat = 'l j M';
+		}
 
 		$days = [];
 		for ($i = 0; $i <= $daycount; $i++)
@@ -42,11 +65,21 @@ class DateSelect extends \formslib_select
 		return $this;
 	}
 
+	/**
+	 * {@inheritDoc}
+	 * @see \formslib_options::getEmailValue()
+	 * @return string
+	 */
 	public function getEmailValue()
 	{
 		return date($this->dateformat, strtotime($this->value));
 	}
 
+	/**
+	 * {@inheritDoc}
+	 * @see \formslib_options::getObjectValue()
+	 * @return \DateTime
+	 */
 	public function &getObjectValue()
 	{
 	    $date = \DateTime::createFromFormat('!Y-m-d', $this->value);
